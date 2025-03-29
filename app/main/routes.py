4f2c92faca62f093d1bcf31325bb02f405f20d15
Sam Tran <epicsanity@hotmail.com>
2025-03-29 19:08:40 +0000

    dashboard update and homepage switched to dasbboard

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,11 +4,11 @@ main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def index():
-    return render_template('index.html')
-
-@main.route('/dashboard')
-def dashboard():
     return render_template('dashboard.html')
+
+@main.route('/index')
+def dashboard():
+    return render_template('index.html')
 
 @main.route('/guide')
 def guides():
